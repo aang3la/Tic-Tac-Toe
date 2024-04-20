d8ad03f603832b0df9088b7a5b01378a712f7bf0
Angela Antova <antovaangela@gmail.com>
2024-04-20 18:08:32 +0200

    game over screen & draw

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Player } from "./components/Player";
 import { GameBoard } from "./components/GameBoard";
 import { Log } from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+import { GameOver } from "./components/GameOver";
 
 const initialGameBoard = [
   [null, null, null],
@@ -55,6 +56,9 @@ export default function App() {
     }
   }
 
+  // The game to start again if we dont have a winner
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
@@ -85,7 +89,7 @@ export default function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {winner && <p>You won, {winner}</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
